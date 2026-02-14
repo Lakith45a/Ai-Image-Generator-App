76b2ef9f55b17d654be5448f9e8a41a051f59817
Lakith Ravindu <it22587756@my.sliit.lk>
2026-02-15 00:10:13 +0530

    feat: Establish initial frontend application structure including dashboard, sidebar, and core UI components.

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -2,13 +2,87 @@ import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
+import { Polar } from "@polar-sh/sdk";
+import { env } from "~/env";
+import { checkout, polar, portal, webhooks } from "@polar-sh/better-auth";
+import { db } from "~/server/db";
+
+const polarClient = new Polar({
+  accessToken: env.POLAR_ACCESS_TOKEN,
+  server: "sandbox",
+});
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "31e2c612-225a-44ad-b134-45dfb6919c98",
+              slug: "classic",
+            },
+            {
+              productId: "a71b976f-dffd-4855-bb1a-c6edd06167d7",
+              slug: "gold",
+            },
+            {
+              productId: "f7568cfb-67ca-4613-a131-1dad79f331dd",
+              slug: "platinum",
+            },
+          ],
+          successUrl: "/dashboard",
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+        webhooks({
+          secret: env.POLAR_WEBHOOK_SECRET,
+          onOrderPaid: async (order) => {
+            const externalCustomerId = order.data.customer.externalId;
+
+            if (!externalCustomerId) {
+              console.error("No external customer ID found.");
+              throw new Error("No external customer id found.");
+            }
+
+            const productId = order.data.productId;
+
+            let creditsToAdd = 0;
+
+            switch (productId) {
+              case "31e2c612-225a-44ad-b134-45dfb6919c98":
+                creditsToAdd = 50;
+                break;
+              case "a71b976f-dffd-4855-bb1a-c6edd06167d7":
+                creditsToAdd = 200;
+                break;
+              case "f7568cfb-67ca-4613-a131-1dad79f331dd":
+                creditsToAdd = 400;
+                break;
+            }
+
+            await db.user.update({
+              where: { id: externalCustomerId },
+              data: {
+                credit: {
+                  increment: creditsToAdd,
+                },
+              },
+            });
+          },
+        }),
+      ],
     }),
-    emailAndPassword: { 
-    enabled: true, 
-  }, 
+  ],
 });
